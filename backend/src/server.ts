140e52bbaeaf2d2d6228b41322a8ffb7146c20bb
Mohamed khaleifa <202300549@pua.edu.eg>
2026-05-14 18:49:02 +0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 5001
 
 connectDB()
 
-app.use(cors({ origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : 'http://localhost:5173', credentials: true }))
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
+}))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
